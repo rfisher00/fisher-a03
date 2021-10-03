ad6ce5c132da5d99902ceacfa89f9bc366ee5e2c
ryanf <ryanfisher00@outlook.com>
2021-10-03 16:35:18 -0400

    first commit: pseudo + skeleton

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Fisher
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/* pseudocode:
+    implement input necessities
+    prompt for the rate of return & store as string
+    use function validInput to check if isNumeric and not 0
+    use validInput boolean to output
+        if notNumeric or rate = 0
+            not valid input
+        else
+            function investment() parses rate to double and returns 72/rate
+ */
+
+public class Solution29 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isNumeric(String str) {
+
+    }
+
+    public static boolean validInput(String rate) {
+
+    }
+
+    public static double investment(String rate) {
+
+    }
+}
---
+++ exercise29/src/test/java/baseline/Solution29Test.java
@@ -0,0 +1,16 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution29Test {
+
+    @Test
+    void validInput() {
+    }
+
+    @Test
+    void investment() {
+    }
+}
