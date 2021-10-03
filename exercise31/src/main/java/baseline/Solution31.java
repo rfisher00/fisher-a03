bd9852348484b45ee28efc0cbe6cb955dad2ca6e
ryanf <ryanfisher00@outlook.com>
2021-10-03 17:53:29 -0400

    final commit

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -18,12 +18,39 @@ import java.util.Scanner;
 public class Solution31 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int age;
-        int restingHR ;
-        System.out.print("How old are you? ");
-        age = sc.nextInt();
-        System.out.print("What is your resting heart rate? (pulses per minute) ");
-        restingHR = sc.nextInt();
+        int age = 0;
+        int restingHR = 0;
+        boolean input = false;
+        while (!input) {
+            try {
+                System.out.print("How old are you? ");
+                age = Integer.valueOf(sc.nextLine());
+                if(age <= 0) {
+                    input = false;
+                    System.out.println("Not a valid number.");
+                }
+                else
+                    input = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Not a valid number.");
+            }
+        }
+
+        input = false;
+        while (!input) {
+            try {
+                System.out.print("What is your resting heart rate? (pulses per minute) ");
+                restingHR = Integer.valueOf(sc.nextLine());
+                if(age <= 0) {
+                    input = false;
+                    System.out.println("Not a valid number.");
+                }
+                else
+                    input = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Not a valid number.");
+            }
+        }
         makeTable(age, restingHR);
     }
 
