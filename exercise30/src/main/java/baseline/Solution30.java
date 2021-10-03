2634b753bd1fa45d469682a53c18831d0a69c083
ryanf <ryanfisher00@outlook.com>
2021-10-03 16:39:31 -0400

    first commit: pseudo + skeleton

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Fisher
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/* pseudocode:
+    implement input necessities
+    in function generateMultiTable():
+        for loop iterating i by 1 from 0 to 12
+            another for loop iterating j from 0 to 12
+                print(i * j) with whitespace (no tabs)
+            print a new line for next row
+ */
+
+public class Solution30 {
+    public static void main(String[] args) {
+
+    }
+    public static void generateMultiTable() {
+
+    }
+}
