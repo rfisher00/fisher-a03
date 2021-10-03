f16cb2c51a383108ef528cab42996d8999575939
ryanf <ryanfisher00@outlook.com>
2021-10-03 16:46:55 -0400

    first commit: pseudo + skeleton

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Fisher
+ */
+
+package baseline;
+
+
+import java.util.Scanner;
+import java.util.Random;
+
+/* pseudocode:
+    implement input necessities
+    prompt user for a question
+    use function eightball with random int
+    to return a random element from array of responses
+    and print to output
+ */
+
+public class Solution33 {
+    public static void main(String[] args) {
+
+    }
+    public static String eightBall() {
+
+    }
+}
