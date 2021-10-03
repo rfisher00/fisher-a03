458db3e0849984821103c6309b1bdb671830ab55
ryanf <ryanfisher00@outlook.com>
2021-10-03 16:00:17 -0400

    first commit: psuedo + skeleton

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Fisher
+ */
+
+package baseline;
+import java.util.Scanner;
+import java.util.Arrays;
+
+/* pseudocode:
+    implement input necessities
+    prompt for the input of 2 strings
+    in function isAnagram;
+    remove all whitespace from strings
+    compare lengths; if equal, continue
+    have each string toCharArray and toLowerCase
+    sort each string array and see if equals() is true
+    use boolean to output if strings are anagrams
+ */
+
+public class Solution24 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isAnagram(String str1, String str2) {
+
+    }
+}
---
+++ exercise24/src/test/java/baseline/Solution24Test.java
@@ -0,0 +1,13 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution24Test {
+
+    @Test
+    void main() {
+
+    }
+}
