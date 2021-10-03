65e3e583ee644ac90ceb6278a4e550a1a50adb62
ryanf <ryanfisher00@outlook.com>
2021-10-03 16:55:20 -0400

    first commit: pseudo + skeleton

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Fisher
+ */
+
+package baseline;
+
+import java.util.*;
+
+/* pseudocode:
+        in function addName()
+            implement input necessities
+            create an empty array of strings
+            use an ArrayList for empty nameList
+            take input in while loop until input == ""
+            have each name add to the Array list
+            convert the ArrayList to an array and return to main
+        in main:
+            create a new random instance
+            have a string array initialized to addName function
+            create a random integer less than the length of the nameArray
+            print out the corresponding element
+ */
+
+public class Solution35 {
+    public static void main(String[] args) {
+
+    }
+    public static String[] addName() {
+
+    }
+}
