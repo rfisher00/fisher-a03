b3cc095057319180640a5c0e19fa2dc46fd6fe66
ryanf <ryanfisher00@outlook.com>
2021-10-03 16:25:21 -0400

    first commit: pseudo + skeleton

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,23 @@
+/*
+*  UCF COP3330 Fall 2021 Assignment 3 Solutions
+*  Copyright 2021 Ryan Fisher
+*/
+
+package baseline;
+import java.util.Scanner;
+
+/* pseudocode:
+    implement input necessities
+    in function summer,  use for loop to obtain multiple integers
+    add each integer to sum = 0 before next iteration
+    return sum and print in main
+ */
+
+public class Solution28 {
+    public static void main(String[] args) {
+
+    }
+    public static int summer() {
+
+    }
+}
