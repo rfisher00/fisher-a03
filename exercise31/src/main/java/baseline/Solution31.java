ad46d1e79bdc990c6590605eddb43ee343ff52c6
ryanf <ryanfisher00@outlook.com>
2021-10-03 16:44:47 -0400

    first commit: pseudo + skeleton

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Fisher
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+/* pseudocode:
+    implement input necessities
+    prompt user for age and heart rate
+    use function makeTable
+    iterating 9 times with i incremented by 5 from 55
+    makeTable uses calctargetHR function to output the correct bpm per intensity i
+ */
+
+public class Solution31 {
+    public static void main(String[] args) {
+
+    }
+
+    public static double calcTargetHR(int age, int restingHR, int intensity) {
+
+    }
+
+    public static void makeTable(int age, int restingHR) {
+
+}
+
