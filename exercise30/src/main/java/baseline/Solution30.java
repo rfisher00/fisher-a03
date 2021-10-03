806513326913dd823082e5536762536556b04ba4
ryanf <ryanfisher00@outlook.com>
2021-10-03 16:40:01 -0400

    final commit

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -17,9 +17,14 @@ import java.util.Scanner;
 
 public class Solution30 {
     public static void main(String[] args) {
-
+        generateMultiTable();
     }
     public static void generateMultiTable() {
-
+        for(int i = 1; i < 13; i++) {
+            for(int j = 1; j < 13; j++) {
+                System.out.printf("%-7d", i*j);
+            }
+            System.out.println("\n");
+        }
     }
 }
