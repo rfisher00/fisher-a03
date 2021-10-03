e180a7fa3c5d12607094b26815ab7ac4f1f292ae
ryanf <ryanfisher00@outlook.com>
2021-10-03 19:08:32 -0400

    first commit: pseudo + skeleton

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,54 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Fisher
+ */
+
+package baseline;
+
+import java.util.*;
+
+/* pseudocode:
+    in function makeList():
+        implement input necessities
+        create an empty array of integers
+        use an ArrayList for empty timeArr
+        take input in while loop until input == "done"
+        parse string input to int and include nonnumeric values exception
+        add each int to the Array list
+        convert the ArrayList to an array and return to main
+    in function output()
+        print out the resulting list of integers
+        use an if statement so the last integer doesn't include a comma, but a newline
+        call on functions findMin, findMax, findAvg and findStd and output their returned values
+ */
+
+public class Solution36 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int findMin(Integer[] timeArr) {
+
+    }
+
+    public static int findMax(Integer[] timeArr) {
+
+    }
+
+    public static double findAvg(Integer[] timeArr) {
+
+    }
+
+    public static double findStd(Integer[] timeArr) {
+
+    }
+
+    public static void output(Integer[] timeArr) {
+
+    }
+
+    public static Integer[] makeList() {
+
+    }
+}
+
