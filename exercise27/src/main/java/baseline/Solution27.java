7fd7d27f0e620c848a771ef43b537bfa138244d1
ryanf <ryanfisher00@outlook.com>
2021-10-03 16:22:17 -0400

    first commit: pseudo + skeleton

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,52 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Fisher
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/* pseudocode:
+
+    implement input necessities
+    prompt for first, last, zip and ID
+    use function validateInput to call the functions
+    each function returns a bool based on validity
+        emptyFirst() & emptyLast(): checks if the field has been filled
+        checkFirst() & checkLast(): checks if input string length >= 2
+        checkId(): checks if length = 7, if first 2 characters are letters, the 3rd is a hyphen, and remaining 4 are #s
+        checkZip(): checks if it is numeric and a length of 5
+
+ */
+
+public class Solution27 {
+    public static void main(String[] args) {
+
+    }
+    public static boolean emptyFirst(String first) {
+
+    }
+    public static boolean emptyLast(String last) {
+
+    }
+    public static boolean checkFirst(String first) {
+
+    }
+
+    public static boolean checkLast(String last) {
+
+    }
+
+    public static boolean checkId(String id) {
+
+    }
+
+    public static boolean checkZip(String zip) {
+
+    }
+
+    public static void validateInput(String first, String last, String id, String zip) {
+
+
+    }
+}
