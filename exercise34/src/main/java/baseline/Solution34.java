49e1ce0a59170a35cc2c170fa481d9b67474e295
ryanf <ryanfisher00@outlook.com>
2021-10-03 16:48:26 -0400

    first commit: pseudo + skeleton

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Fisher
+ */
+
+package baseline;
+
+import java.util.Scanner;
+import java.util.Random;
+
+/* pseudocode:
+    in function employeeList:
+    initialize array employees with 5 names
+    output the # of employees/elements
+    prompt for who to remove
+    iterate thru array until name matches employee[i]
+    save i as index
+    create a new array of one less length
+    iterate through array again, but continue if index == i
+    and have remaining employees put in the new array
+    return the new array to main
+    in main: print length of new array and its elements
+ */
+
+public class Solution34 {
+    public static void main(String[] args) {
+
+        }
+    }
+    public static String[] employeeList() {
+
+    }
+}
+
+
