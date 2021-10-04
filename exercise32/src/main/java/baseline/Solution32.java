fac28b281d87c97b91bbe78fa470fb9786285028
ryanf <ryanfisher00@outlook.com>
2021-10-03 22:44:38 -0400

    final commit

--- exercise32/src/main/java/baseline/Solution32.java
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -40,10 +40,51 @@ import java.util.Random;
 public class Solution32 {
     public static void main(String[] args) {
 
+        System.out.println("Let's play Guess the Number!");
+        numberGuesser();
     }
 
     public static void numberGuesser() {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        boolean running = true;
+        while(running) {
+            System.out.printf("%nEnter the difficulty level (1, 2, or 3): ");
+            int difficulty = sc.nextInt();
+            int random = 7;
+            if(difficulty == 1)
+                random = rand.nextInt(11);
+            if(difficulty == 2)
+                random = rand.nextInt(101);
+            if(difficulty == 3)
+                random = rand.nextInt(1001);
+            System.out.print("I have my number. What's your guess? ");
+            int guess = sc.nextInt();
+            int guesses = 1;
+            boolean correct = false;
+            while(!correct) {
+                if(guess == random) {
+                    System.out.printf("You got it in %d guesses!%n", guesses);
+                    correct = true;
+                }
+                else {
+                    if(guess < random) {
+                        System.out.printf("Too low. Guess again: ");
+                        guesses++;
+                        guess = sc.nextInt();
+                    }
+                    if(guess > random) {
+                        System.out.printf("Too high. Guess again: ");
+                        guesses++;
+                        guess = sc.nextInt();
+                    }
+                }
+            }
 
+            System.out.print("Do you wish to play again (Y/N)? ");
+            String response = sc.next();
+            if(response.equals("n") || response.equals("N"))
+                running = false;
         }
     }
 }
