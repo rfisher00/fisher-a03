21b2d8c2f6462e024afecc44bf0136ad6516e80a
ryanf <ryanfisher00@outlook.com>
2021-10-03 21:52:00 -0400

    first commit: pseudo + skeleton

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ryan Fisher
+ */
+
+package baseline;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+/* pseudocode:
+    implement input necessities
+    prompt for numbers separated by spaces
+    save as a string and pass it into filterEvenNumbers()
+
+    in function filterEvenNumbers()
+        create a new empty string arr
+        check if passed in field is empty
+        if not:
+            have the empty arr initialized as the passed in string.split(" ")
+            iterate through the array
+            parse each character into an int and
+            if int number % 2 = 0:
+                isEven, print
+ */
+
+public class Solution38 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void filterEvenNumbers(String numbers) {
+
+    }
+}
